504d1b46ac9bfd4985e3e933509ef8b646b9282f
Elton Santos <eltonaxl@hotmail.com>
2023-07-29 04:23:38 -0300

    Tailwind-merge

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,11 @@ import { Button } from "./components/Button";
 
 export function App() {
   return (
-    <div className="flex h-screen items-center justify-center bg-zinc-950 text-zinc-50">
+    <div className="flex h-screen items-center justify-center gap- bg-zinc-950 text-zinc-50 flex-col">
+      <Button success className="w-20">Sign in</Button>
+      <Button>Sign in</Button>
+      <Button>Sign in</Button>
+      <Button>Sign in</Button>
       <Button>Sign in</Button>
     </div>
   )
--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -1,15 +1,16 @@
 import { ComponentProps } from "react"
 import { CheckCircle } from "lucide-react"
+import { twMerge } from "tailwind-merge"
 
-type ButtonProps = ComponentProps<'button'> & {
+export type ButtonProps = ComponentProps<'button'> & {
   success?: boolean
 }
 
-export function Button({ success = false, ...props }: ButtonProps) {
+export function Button({ success = false, className, ...props }: ButtonProps) {
   return (
     <button
       data-success={success}
-      className="rounded bg-zinc-50 px-4 h-10 text-sm font-medium text-zinc-900 hover:bg-zinc-200 data-[success=true]:bg-emerald-500 data-[success=true]:hover:bg-emerald-600"
+      className={twMerge("flex justify-center items-center rounded bg-zinc-50 px-4 h-10 text-sm font-medium text-zinc-900 hover:bg-zinc-200 data-[success=true]:bg-emerald-500 data-[success=true]:hover:bg-emerald-600", className)}
       {...props}
     >
       {success ? <CheckCircle className="h-4 w-4" /> : props.children}
