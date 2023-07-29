ca756a383756d995324b16afd25cf08a1b715885
Elton Santos <eltonaxl@hotmail.com>
2023-07-29 04:36:51 -0300

    Tailwind-variants

--- src/App.tsx
+++ src/App.tsx
@@ -2,10 +2,10 @@ import { Button } from "./components/Button";
 
 export function App() {
   return (
-    <div className="flex h-screen items-center justify-center gap- bg-zinc-950 text-zinc-50 flex-col">
+    <div className="flex h-screen items-center justify-center gap-6 bg-zinc-950 text-zinc-50 flex-col">
       <Button success className="w-20">Sign in</Button>
-      <Button>Sign in</Button>
-      <Button>Sign in</Button>
+      <Button size="sm">Sign in</Button>
+      <Button size="xs">Sign in</Button>
       <Button>Sign in</Button>
       <Button>Sign in</Button>
     </div>
--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -1,18 +1,31 @@
 import { ComponentProps } from "react"
 import { CheckCircle } from "lucide-react"
 import { twMerge } from "tailwind-merge"
+import { tv, VariantProps } from "tailwind-variants"
 
-export type ButtonProps = ComponentProps<'button'> & {
-  success?: boolean
-}
+const button = tv({
+  base: "flex justify-center items-center rounded bg-zinc-50 text-sm font-medium text-zinc-900 hover:bg-zinc-200 data-[success=true]:bg-emerald-500 data-[success=true]:hover:bg-emerald-600",
+  variants: {
+    size: {
+      default: 'px-4 h-10',
+      sm: 'px-3 h-8',
+      xs: 'px-2 h-6 text-xs'
+    },
+    success: {
+      true: "bg-emerald-500 hover:bg-emerald-600"
+    },
+  },
+  defaultVariants: {
+    size: 'default',
+    success: false
+  }
+})
 
-export function Button({ success = false, className, ...props }: ButtonProps) {
+export type ButtonProps = ComponentProps<'button'> & VariantProps<typeof button>
+
+export function Button({ success, size, className, ...props }: ButtonProps) {
   return (
-    <button
-      data-success={success}
-      className={twMerge("flex justify-center items-center rounded bg-zinc-50 px-4 h-10 text-sm font-medium text-zinc-900 hover:bg-zinc-200 data-[success=true]:bg-emerald-500 data-[success=true]:hover:bg-emerald-600", className)}
-      {...props}
-    >
+    <button data-success={success} className={button({ size, success, className })} {...props}>
       {success ? <CheckCircle className="h-4 w-4" /> : props.children}
     </button>
   )
